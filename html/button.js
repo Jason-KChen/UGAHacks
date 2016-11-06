89d4f1c216caa42484f8a6032c3b5526d81b91c9
qqtan36 <yqtan36@gmail.com>
2016-11-05 23:13:29 -0400

    added button.js

---
+++ html/button.js
@@ -0,0 +1,9 @@
+
+       $("button").click(function(){
+           $(this).replaceWith(
+        "<form style='' class='navbar-form navbar-left'>"+
+        "<div class='form-group'>"+
+         " <input type='text' class='form-control' placeholder='Search'>"+
+        "</div>"+
+        "<button type='submit' class='btn btn-default'>"+"</button>"+
+      "</form>")});
