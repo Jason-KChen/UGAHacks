4cac7ee31cdbc6ef32d748879415d7a788cd41cd
qqtan36 <yqtan36@gmail.com>
2016-11-06 03:48:12 -0500

    Added autocomplete for selecting airport+update current airport

--- html/button.js
+++ html/button.js
@@ -1,16 +1,25 @@
 
-       $("button").click(function(){
-           $(this).replaceWith(
-        "<form style='' class='navbar-form navbar-left'>"+
-        "<div class='form-group'>"+
-         " <input type='text' class='form-control' style='width: 50%; margin: auto' placeholder='Search'>"+
-        "</div>"+
-        "<button type='submit' class='btn btn-default'>"+'Submit'+"</button>"+
-      "</form>")});
-
+$("#getBtn").click(function(){
+//           $(this).replaceWith(
+//        "<form style='' class='navbar-form navbar-left'>"+
+//        "<div class='form-group'>"+
+//         " <input type='text' class='form-control' style='width: 50%; margin: auto' placeholder='Search'>"+
+//        "</div>"+
+//        "<button type='submit' class='btn btn-default'>"+'Submit'+"</button>"+
+//      "</form>")
+        $('.overlay').show("slow");
+        $('#getBtn').hide();
+});
+$("#submit").click(function(){
+        $('.overlay').hide();
+        $('#getBtn').show();
+        var airport = $('.form-control').val();
+        alert(airport);
+        $('#airport').html(airport);
+});
     $(function(){
-        var list = ["Los Angeles", "New York", "Las Vegas", "chicago", "san diego", "washington", "orlando", "houston", "seattle", "san antonio", "miami", "boston", "atlanta" ];
+        var list = ["LAX", "JFK", "LAS", "ORD", "SAN", "DCA", "MCO", "IAH", "SEA", "SAT", "MIA", "BOS", "ATL" ];
         $( ".form-control" ).autocomplete({
       source: list
     });
-    });
+});
